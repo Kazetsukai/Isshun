147e290c8f908ea235628ec6fd1478a6f20b1197
joejimufu <righteouslyalgebraic@gmail.com>
2015-02-01 20:32:02 +1300

    dumping my stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+
+	//Player handling
+	public float gravity = 20;
+	public float speed = 8;
+	public float acceleration = 30;
+
+	public float jumpHeight = 12;
+
+	float currentSpeed;
+	float targetSpeed;
+	Vector2 amountToMove;
+
+	PlayerPhysics playerPhysics;
+
+	void Start () 
+	{
+		playerPhysics = GetComponent<PlayerPhysics>();
+	}
+
+	void Update () 
+	{	
+		// Reset acceleration upon collision
+		if (playerPhysics.movementStopped) {
+			targetSpeed = 0;
+			currentSpeed = 0;
+		}
+
+		if (playerPhysics.grounded)
+		{
+			amountToMove.y = 0;
+
+			//Jump
+			if (Input.GetButtonDown("Jump"))
+			{
+				amountToMove.y = jumpHeight;
+			}
+		}
+		//Input
+		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+		currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+
+		//Set amount to move
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move(amountToMove * Time.deltaTime);
+	}
+
+	float IncrementTowards(float n, float target, float a)
+	{
+		if (n == target)
+		{
+			return n;
+		}
+		else
+		{
+			float dir = Mathf.Sign(target - n);
+			n += a * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(target - n))? n: target;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent (typeof(BoxCollider2D))]
+public class PlayerPhysics : MonoBehaviour {
+	
+	public LayerMask collisionMask;
+	
+	private BoxCollider2D collider;
+	private Vector3 size;
+	private Vector3 center;
+	
+	private float skin = .005f;
+	
+	[HideInInspector]
+	public bool grounded;
+
+	[HideInInspector]
+	public bool movementStopped;
+	
+	Ray ray;
+	RaycastHit hit;
+	
+	void Start() {
+		collider = GetComponent<BoxCollider2D>();
+		size = collider.size;
+		center = collider.center;
+	}
+	
+	public void Move(Vector2 moveAmount) {
+		
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 position = transform.position;
+		
+		// Check collisions above and below
+		grounded = false;
+		
+		for (int i = 0; i<3; i ++) {
+			float dir = Mathf.Sign(deltaY);
+			float x = (position.x + center.x - size.x/2) + size.x/2 * i; // Left, centre and then rightmost point of collider
+			float y = position.y + center.y + size.y/2 * dir; // Bottom of collider
+			
+			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+			Debug.DrawRay(ray.origin,ray.direction);
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaY),collisionMask)) {
+				// Get Distance between player and ground
+				float dst = Vector3.Distance (ray.origin, hit.point);
+				
+				// Stop player's downwards movement after coming within skin width of a collider
+				if (dst > skin) 
+				{
+					deltaY = dst * dir + skin;
+				}
+				else 
+				{
+					deltaY = 0;
+				}
+				
+				grounded = true;				
+				break;				
+			}
+		}		
+
+		// Check collisions left and right
+		movementStopped = false;
+		for (int i = 0; i<3; i ++) 
+		{
+			float dir = Mathf.Sign(deltaX);
+			float x = position.x + center.x + size.x/2 * dir;
+			float y = position.y + center.y - size.y/2 + size.y/2 * i;
+			
+			ray = new Ray(new Vector2(x,y), new Vector2(dir,0));
+			Debug.DrawRay(ray.origin,ray.direction);
+			
+			if (Physics.Raycast(ray,out hit,Mathf.Abs(deltaX) + skin,collisionMask)) 
+			{
+				// Get Distance between player and ground
+				float distance = Vector3.Distance (ray.origin, hit.point);
+				
+				// Stop player's downwards movement after coming within skin width of a collider
+				if (distance > skin) 
+				{
+					deltaX = distance * dir - skin * dir;
+				}
+				else 
+				{
+					deltaX = 0;
+				}
+				
+				movementStopped = true;
+				break;				
+			}
+		}		
+		
+		Vector2 finalTransform = new Vector2(deltaX,deltaY);		
+		transform.Translate(finalTransform);
+	}	
+}
